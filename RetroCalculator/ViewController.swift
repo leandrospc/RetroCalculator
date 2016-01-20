bbc120d425ee14f8980dbff455374a26b774754a
leandrospc <leandropc@gmail.com>
2016-01-19 21:54:28 -0600

    Retro Calculator is done and ready to commit to GitHub

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,113 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operacao: String {
+        case soma = "+"
+        case subtrai = "-"
+        case multiplica = "*"
+        case divide = "/"
+        case vazio = "Vazio"
+        
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var btAudioPlayer: AVAudioPlayer!
+    
+    var numeroCorrente = ""
+    var numeroDireta = ""
+    var numeroEsquerda = ""
+    var operacaoAtual: Operacao = Operacao.vazio
+    var resultado = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do{
+            try btAudioPlayer = AVAudioPlayer(contentsOfURL: soundURL)
+            btAudioPlayer.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func botaoPressionado(btn: UIButton){
+        playSound()
+        numeroCorrente += "\(btn.tag)"
+        resultLabel.text = numeroCorrente
     }
 
+    func playSound(){
+        if btAudioPlayer.playing {
+            btAudioPlayer.stop()
+        }
+        
+        btAudioPlayer.play()
+    }
 
+    @IBAction func somaPressionado(sender: AnyObject) {
+        operacoes(Operacao.soma)
+    }
+    
+    @IBAction func menosPressionado(sender: AnyObject) {
+        operacoes(Operacao.subtrai)
+    }
+    
+    @IBAction func divididoPressionado(sender: AnyObject) {
+        operacoes(Operacao.divide)
+    }
+    
+    @IBAction func multiPressionado(sender: AnyObject) {
+        operacoes(Operacao.multiplica)
+    }
+    
+    @IBAction func igualPressionado(sender: AnyObject) {
+        operacoes(operacaoAtual)
+    }
+    
+    func operacoes(op: Operacao){
+        playSound()
+        if operacaoAtual != Operacao.vazio{
+            
+            if numeroCorrente != ""{
+                
+                numeroDireta = numeroCorrente
+                numeroCorrente = ""
+                
+                if operacaoAtual == Operacao.soma {
+                    resultado = "\(Double(numeroEsquerda)! + Double(numeroDireta)!)"
+                }
+                if operacaoAtual == Operacao.subtrai {
+                    resultado = "\(Double(numeroEsquerda)! - Double(numeroDireta)!)"
+                }
+                if operacaoAtual == Operacao.multiplica {
+                    resultado = "\(Double(numeroEsquerda)! * Double(numeroDireta)!)"
+                }
+                if operacaoAtual == Operacao.divide {
+                    resultado = "\(Double(numeroEsquerda)! / Double(numeroDireta)!)"
+                }
+                
+                numeroEsquerda = resultado
+                
+                resultLabel.text = resultado
+            }
+                
+            operacaoAtual = op
+            
+        }else {
+            numeroEsquerda = numeroCorrente
+            numeroCorrente = ""
+            operacaoAtual = op
+        }
+    }
 }
 
